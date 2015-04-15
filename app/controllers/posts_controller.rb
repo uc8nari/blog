8140e462fd49a23d20badfd39bd472314e04dbb1
江藤雄思 <num8.4645@gmail.com>
2015-04-15 14:02:38 +0900

    first commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,44 @@
+class PostsController < ApplicationController
+    
+    def new
+        @post = Post.new
+    end
+    
+    def create
+        @post = Post.new(params[:post].permit(:title, :text))
+        if @post.save
+            redirect_to @post
+        else
+            render 'new'
+        end
+    end
+    
+    def show
+    @post = Post.find(params[:id])
+    end
+    
+    def index
+    @posts = Post.all
+    end
+    
+    def edit
+    @post = Post.find(params[:id])
+    end
+    
+    def update
+        @post = Post.find(params[:id])
+        if @post.update(params[:post].permit(:title, :text))
+        redirect_to @post
+        else
+        render 'edit'
+    end
+    end
+    
+    
+    private
+  def post_params
+    params.require(:post).permit(:title, :text)
+    end
+    
+
+end
